cd794f287e7df2876fedfbb1d9b7d022eabb8982
Rithvik Nishad <rithvikn2001@gmail.com>
2020-09-10 13:55:22 +0000

    updt

--- bin/main.dart
+++ bin/main.dart
@@ -1,28 +1,3 @@
-// import 'package:bakecode/framework/bakecode.dart';
+import 'package:bakecode/framework/bakecode.dart';
 
-// main() => BakeCodeRuntime.instance.run();
-
-import 'dart:io';
-
-import 'package:yaml/yaml.dart';
-
-Map loadYamlFileSync(String path) {
-  File file = new File(path);
-  if (file?.existsSync() == true) {
-    return loadYaml(file.readAsStringSync());
-  }
-  return null;
-}
-
-Future<Map> loadYamlFile(String path) async {
-  File file = new File(path);
-  if ((await file?.exists()) == true) {
-    String content = await file.readAsString();
-    return loadYaml(content);
-  }
-  return null;
-}
-
-main(List<String> args) {
-  print(loadYamlFileSync("pubspec.yaml"));
-}
+main() => BakeCodeRuntime.instance.run();
