b00d9708f2bc0e05a5bfaef763240305a6083e3c
Rithvik Nishad <rithvikn2001@gmail.com>
2020-11-16 18:50:37 +0530

    update node

--- lib/src/jobs/node.dart
+++ lib/src/jobs/node.dart
@@ -2,30 +2,24 @@ import 'package:bakecode/src/jobs/connection.dart';
 import 'package:bakecode/src/jobs/jobs.dart';
 import 'package:meta/meta.dart';
 
-/// A connectable element with multiple inputs and single output.
+/// A connectable element.
 abstract class Node {
-  /// All inbound connections to this node.
-  @nonVirtual
-  final inputs = <Connection>[];
-
   /// The outbound connection from this node.
   @nonVirtual
   final output = Connection();
 
-  /// Connects the instance node to the specified [to] node.
+  /// Connects the instance node to the specified [node].
   ///
-  /// For `A` as the instance node and `B` as the specified [to] node,
+  /// For `A` as the instance node and `B` as the specified [node],
   /// ```dart
   /// A.connect(B);
   /// ```
-  /// makes a new **forward connection** from `A` to `B`, such that `A`'s
-  /// [output] is added to `B`'s [input].
-  ///
-  /// `A` --> `B`
+  /// makes a new **forward connection** from `A` to `B`, such that `B` listens
+  /// to `A`'s [output] connection.
   ///
   /// To make a forward conection to multiple nodes, use [connectToAll].
   @nonVirtual
-  void connectTo(Node to) => to.inputs.add(output);
+  void connectTo(Node node) => output.addEndPoint(node);
 
   /// Makes a forward connection from this node to all [nodes].
   void connectToAll(List<Node> nodes) => nodes.map(connectTo);
