7ee6cd08e487380d399abdd148b684283e2b7ff4
Rithvik Nishad <rithvikn2001@gmail.com>
2020-11-18 21:53:07 +0530

    fixed

--- lib/src/jobs/connection.dart
+++ lib/src/jobs/connection.dart
@@ -5,18 +5,25 @@ import 'package:meta/meta.dart';
 
 import 'flow_context.dart';
 
+/// This asbtract class gives access to the stream of the [Connection].
 @immutable
 abstract class InputConnection {
+  /// The listenable stream of the [Connection._flow].
   Stream<FlowContext> get stream;
 }
 
+/// This abstract class presents functionalities to connect the output to
+/// another node gives access to the sink of the [Connection].
 @immutable
 abstract class OutputConnection {
+  /// The sink of the [Connection._flow].
   Sink<FlowContext> get sink;
 
+  /// Makes a forward connection to a Node.
   void connectTo(Node node);
 }
 
+/// The element that links multiple [Node]s.
 @immutable
 class Connection with InputConnection, OutputConnection {
   final _flow = StreamController<FlowContext>();
@@ -25,10 +32,10 @@ class Connection with InputConnection, OutputConnection {
   Sink<FlowContext> get sink => _flow.sink;
 
   @override
-  Stream<FlowContext> get stream => _flow.stream;
+  Stream<FlowContext> get stream => _flow.stream.asBroadcastStream();
 
   final awaiters = <Node>[];
 
   @override
-  void connectTo(Node receiver) => awaiters.add(receiver..listenTo(this));
+  void connectTo(Node receiver) => awaiters.add(receiver..await(this));
 }
--- lib/src/jobs/node.dart
+++ lib/src/jobs/node.dart
@@ -1,49 +1,78 @@
 import 'dart:async';
 
-import 'package:bakecode/bakecode.dart';
 import 'package:bakecode/src/jobs/connection.dart';
 import 'package:bakecode/src/jobs/jobs.dart';
 import 'package:meta/meta.dart';
 
+// TODO: add progress awaition for flowcontext specials... make use of listen.onDone callback
 /// A connectable element.
-class Node extends Service {
-  final inputs = <InputConnection>[];
+abstract class Node {
+  /// Contains the received [FlowContext] for every [InputConnection] connected
+  /// to the instance [Node].
+  ///
+  /// [inputContexts.values] can contain null if a [FlowContext] was not
+  /// received from an [InputConnection] in [inputs].
+  final inputContexts = <InputConnection, FlowContext>{};
 
-  final outputs = <OutputConnection>[];
+  /// Every [InputConnection]s the [Node] listens to.
+  Iterable<InputConnection> get inputs => inputContexts.keys;
 
-  Sink<FlowContext> get sink => _flowController.sink;
+  /// The [OutputConnection] of this instance [Node].
+  ///
+  /// [Connection] closes once [run] has successfully completed.
+  final OutputConnection output = Connection();
 
-  final _flowController = StreamController<FlowContext>();
+  /// Evaluates to true if received a [FlowContext] from every [inputs] of the
+  /// instance node else false.
+  ///
+  /// Checks if [inputContexts.values] contains null. [inputContexts.values]
+  /// is null if a [FlowContext] was not received from an [InputConnection] in
+  /// [inputs].
+  bool get receivedAll => inputContexts.values.contains(null) == false;
 
-  Node() {
-    _flowController.stream;
+  /// Start's listening to [input] for [FlowContext].
+  void await(InputConnection input) {
+    inputContexts[input] = null;
+
+    input.stream.listen((context) async {
+      inputContexts[input] = context;
+
+      if (receivedAll) {
+        output.sink
+          ..add(await run(context))
+          ..close();
+        // TODO: merge contextsss of every first elements
+      }
+    });
   }
 
   /// Connects the instance node to the specified [node].
   ///
   /// For `A` as the instance node and `B` as the specified [node],
   /// ```dart
-  /// A.connect(B);
+  /// A.connectTo(B);
   /// ```
   /// makes a new **forward connection** from `A` to `B`, such that `B` listens
   /// to `A`'s [output] connection.
   ///
   /// To make a forward conection to multiple nodes, use [connectToAll].
   @nonVirtual
-  void connectTo(Node node) => output.addEndPoint(node);
+  void connectTo(Node node) => output.sink;
 
   /// Connects the instance node to every [Node] in [nodes].
   ///
-  /// For `A` as the instance node and `B` as a Node in [nodes],
+  /// For `A` as the instance node and [nodes] as a collection of [Node]s,
   /// ```dart
-  /// A.connect(B);
+  /// A.connectToAll(nodes);
   /// ```
-  /// makes a new **forward connection** from `A` to `B`, such that `B` listens
-  /// to `A`'s [output] connection.
+  /// makes a new **forward connection** from `A` to every node in [nodes],
+  /// such that every [Node] in [nodes] listens to `A`'s [output] connection.
   ///
   /// To make a forward conection to a single node, use [connectTo].
   @nonVirtual
   void connectToAll(List<Node> nodes) => nodes.map(connectTo);
 
+  /// Executed when all inputs gives a
+  @protected
   Future<FlowContext> run(FlowContext context);
 }
