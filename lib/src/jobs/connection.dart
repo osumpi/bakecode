1e3fabc813e77ee8d7d2fbad8f89c8e36403c01a
Rithvik Nishad <rithvikn2001@gmail.com>
2020-11-18 17:03:52 +0530

    #2 update connection.dart

--- lib/src/jobs/connection.dart
+++ lib/src/jobs/connection.dart
@@ -6,15 +6,29 @@ import 'package:meta/meta.dart';
 import 'flow_context.dart';
 
 @immutable
-@sealed
-class Connection {
+abstract class InputConnection {
+  Stream<FlowContext> get stream;
+}
+
+@immutable
+abstract class OutputConnection {
+  Sink<FlowContext> get sink;
+
+  void connectTo(Node node);
+}
+
+@immutable
+class Connection with InputConnection, OutputConnection {
   final _flow = StreamController<FlowContext>();
-}
 
-class InputConnection extends Connection {
-  Stream<FlowContext> get stream => _flow.stream;
-}
-
-class OutputConnection extends Connection {
+  @override
   Sink<FlowContext> get sink => _flow.sink;
+
+  @override
+  Stream<FlowContext> get stream => _flow.stream;
+
+  final awaiters = <Node>[];
+
+  @override
+  void connectTo(Node receiver) => awaiters.add(receiver..listenTo(this));
 }
