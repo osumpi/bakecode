1ec590d19950b77c833869c454815f16f22306da
Rithvik Nishad <rithvikn2001@gmail.com>
2021-08-06 22:17:01 +0530

    implement engine

--- lib/src/engine.dart
+++ lib/src/engine.dart
@@ -1,1 +1,36 @@
 part of bakecode.engine;
+
+class BakeCodeEngine {
+  /// This class shall not be instantiated more than once.
+  BakeCodeEngine._();
+
+  /// The instance of BakeCode Engine.
+  static late final instance = BakeCodeEngine._();
+
+  /// Whether the engine is initialized or not. Avoid accessing other members
+  /// of this instance if the value is `false`.
+  ///
+  /// See also:
+  /// * [initialize], to initialize the engine.
+  bool isInitialized = false;
+
+  /// Initializes the engine instance by loading the ecosystem configuration,
+  /// initializing the BSI layer, and registering mDNS discovery.
+  Future<bool> initialize() async {
+    ecosystem = await Ecosystem.loadFromFile();
+
+    await BSI.initialize(
+      const BSIConfig(
+        name: 'eni',
+        server: '192.168.43.20',
+        port: 1883,
+        username: 'user',
+        password: 'iL0v3MoonGaYoung',
+      ),
+    );
+
+    return isInitialized = true;
+  }
+
+  late final Ecosystem ecosystem;
+}
