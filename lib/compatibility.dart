23596f1c938b609c36dd66ac2edf81dc2e8da10a
Aswin Murali <aswinmurali.co@gmail.com>
2021-08-09 23:31:44 +0530

    checks dart, git, curl

--- lib/compatibility.dart
+++ lib/compatibility.dart
@@ -6,19 +6,37 @@ import 'package:bakecode_engine/logging.dart';
 class BakeCodeCompatibility {
   const BakeCodeCompatibility._();
 
-  /// Checks if git is available in the system.
-  Future<bool> checkGit() async {
+  /// Checks if a command can be [executable] with [arguments] and [errorMsg]
+  /// if the command fails.
+  Future<bool> _check(
+      String executable, List<String> arguments, String errorMsg) async {
     try {
-      await Process.run('git', ['--version']);
+      await Process.run(executable, arguments);
     } on ProcessException {
-      log('git is not installed. Please install it');
+      log(errorMsg);
       return false;
     }
     return true;
   }
 
+  /// Checks if git is available in the system.
+  Future<bool> checkGit() async =>
+      _check('git', ['--version'], 'git is not installed. Please install it!');
+
+  /// Checks if dart is available in the system.
+  Future<bool> checkDart() async =>
+      _check('dart', ['--version'], 'dart is not installed. Please install it!');
+
+  /// Checks if curl is available in the system.
+  Future<bool> checkCurl() async =>
+      _check('curl', ['--version'], 'curl is not installed. Please install it!');
+
   Future<bool> init() async {
-    return ![await checkGit()].any((checks) => false);
+    return ![
+      await checkGit(),
+      await checkCurl(),
+      await checkDart(),
+    ].any((checks) => false);
   }
 }
 
