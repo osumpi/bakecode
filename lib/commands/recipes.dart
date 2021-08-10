df57f99335b1857a1e8f01f1d058d71b29f7cc28
Rithvik Nishad <rithvikn2001@gmail.com>
2021-08-10 19:17:54 +0530

    adds recipes command

---
+++ lib/commands/recipes.dart
@@ -0,0 +1,17 @@
+part of bakecode.engine;
+
+class RecipesCommand extends Command {
+  @override
+  String get name => 'recipes';
+
+  @override
+  String get description => """
+Recipe manager.
+This requires the recipe manager service running in the ecosystem.
+""";
+
+  @override
+  void run() {
+    print(argResults!.arguments);
+  }
+}
