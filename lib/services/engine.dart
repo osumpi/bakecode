e00968babb2908fdaa16ef8f46d718b4d208b08b
Rithvik Nishad <rithvikn2001@gmail.com>
2021-08-21 20:58:48 +0530

    adds command parser instantiated

--- lib/services/engine.dart
+++ lib/services/engine.dart
@@ -9,6 +9,59 @@ class BakecodeEngine extends Service {
   Future<void> initState() async {
     super.initState();
 
+    runner.addCommand(_InstantiatedCommand());
+
     ecosystem.set(await Ecosystem.loadFromFile());
+  }
+
+  @override
+  void onMessageReceived(ServiceMessage message) {
+    super.onMessageReceived(message);
+
+    try {
+      runner.run(message.arguments);
+    } catch (exception) {
+      log('$exception');
+    }
+  }
+
+  late final runner = CommandRunner(config.name, config.description);
+}
+
+class _InstantiatedCommand extends Command {
+  final name = 'instantiated';
+  final description = '';
+
+  _InstantiatedCommand() {
+    addSubcommand(_InstantiatedServiceCommand());
+  }
+
+  run() {
+    return super.run();
+  }
+}
+
+class _InstantiatedServiceCommand extends Command {
+  @override
+  final name = 'service';
+
+  @override
+  final description = '';
+
+  _InstantiatedServiceCommand() {
+    argParser.addOption('name', mandatory: true);
+    argParser.addOption('id', mandatory: true);
+    argParser.addOption('address', mandatory: true);
+  }
+
+  @override
+  FutureOr? run() {
+    final name = argResults!['name'] as String;
+    final id = argResults!['id'] as String;
+    final address = Address(argResults!['address'] as String);
+
+    // TODO: invoke ecosystem.addService(name, id, address);
+
+    return super.run();
   }
 }
