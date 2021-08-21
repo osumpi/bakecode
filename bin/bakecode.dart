85e3e995f41a6cb045da7cd69cc26e08f287e7b0
Rithvik Nishad <rithvikn2001@gmail.com>
2021-08-21 22:27:42 +0530

    rm test code in main

--- bin/bakecode.dart
+++ bin/bakecode.dart
@@ -1,24 +1,12 @@
 import 'dart:async';
-import 'dart:convert';
 import 'dart:io';
 
 import 'package:args/command_runner.dart';
 import 'package:bakecode_engine/bakecode.dart';
-
 import 'package:bakecode_engine/logging.dart' as logger;
-import 'package:core/core.dart';
 
 Future<void> main(List<String> args) async {
   logger.initialize();
-
-  final ecosystem = await Ecosystem.loadFromFile();
-
-  ecosystem.merge(
-      Address('something/inside/led'), 'ea3b800f-f148-41ff-b558-e4807f13ce63');
-
-  print(const JsonEncoder.withIndent('  ').convert(ecosystem));
-
-  //Ecosystem.recursivelyAddValue()
 
   final runner = CommandRunner('bakecode', description);
 
